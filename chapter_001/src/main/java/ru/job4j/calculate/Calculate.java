1afaa7b15f61b386324b6c08d01661e949e8059b
filonich-vlad <filonich.v@mail.ru>
2018-12-10 23:39:10 +0300

    Задание 1.3. JavaDoc. Code Convention [#94522]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ *Class Calculate решение задачи части 1.3. JavaDoc.
+ *@Author filonich
+ *@since 09.12.2018
+*/
 public class Calculate {
 	
+	/**
+	 * main.
+	 * @param args - некий строковый массив.
+	 * @return "Hello, world!".
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package для задания 1.3. JavaDoc.
+*
+* @author Vladislav Filonich
+* @version 1
+* @since 10.12.2018
+*/
+package ru.job4j.calculate;
