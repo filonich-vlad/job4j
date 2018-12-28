33c2f4fb4c22f8b5512649f81db46f1163ad607e
filonich-vlad <filonich.v@mail.ru>
2018-12-28 19:06:45 +0300

    4.1. Глупый бот. Patched.

---
+++ chapter_001/src/main/java/ru/job4j/dumbot/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.dumbot;
+
+/**
+ * @author Vladislav Filonich
+ * @version $Id$
+ * @since 28.12.18
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в турик. Спровите что-нибудь другое.";
+        if ("Привет, бот.".equals(question)) {
+            rsl = "Привет, умник!";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи!";
+        }
+        return rsl;
+     }
+}
---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,11 @@
+package ru.job4j.max;
+
+/**
+ * Вычисчляет максимум из first и second.
+ */
+
+public class Max {
+    public int max(int first, int second) {
+        return (first > second) ? first : second;
+    }
+}
