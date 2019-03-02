7222308dfb50d61c9b314fd7476a69b0595595a3
filonich-vlad <filonich.v@mail.ru>
2019-03-02 11:10:16 +0300

    2. Создания Manifest для проекта Tracker[#94559]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,51 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class StartUITest {
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
+    }
+
+    @Test
+    public void whenDeleteThenItemDisapears() {
+        Tracker tracker = new Tracker();
+        Item item2 = tracker.add(new Item("i am gonna be deleted", "desc"));
+        Input input = new StubInput(new String[]{"3", item2.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll().length, is(0));
+    }
+    @Test
+    public void whenFindByIdThenItWorksFine() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getId(), is(item.getId()));
+    }
+
+    @Test
+    public void whenFindByNameThenItWorks() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"5", "test name", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findByName(item.getName())[0].getName(), is("test name"));
+    }
+
+}
