cf1ba091ff50f09cacbe5c11ca317a36153b9db4
filonich-vlad <filonich.v@mail.ru>
2019-03-15 13:22:50 +0300

    5. Рефакторинг теста @Before @After[#94557]

--- chapter_002/src/test/java/ru/job4j/shapes/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/shapes/PaintTest.java
@@ -2,16 +2,32 @@ package ru.job4j.shapes;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
 
 public class PaintTest {
+
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("Execute before each test...");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("Execute after each test...");
+    }
+
     @Test
-    public void whenPaint() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+    public void whenPaintSquare() {
         new Paint().draw((new Square()));
         assertThat(new String(out.toByteArray()),
                 is(
@@ -27,6 +43,24 @@ public class PaintTest {
                         .append(System.getProperty("line.separator"))
                         .toString()
                 ));
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenPaintTriangle() {
+        new Paint().draw((new Triangle()));
+        assertThat(new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("   +   ")
+                                .append(System.getProperty("line.separator"))
+                                .append("  + +  ")
+                                .append(System.getProperty("line.separator"))
+                                .append(" +   + ")
+                                .append(System.getProperty("line.separator"))
+                                .append("+++++++")
+                                .append(System.getProperty("line.separator"))
+                                .append(System.getProperty("line.separator"))
+                                .toString()
+                ));
     }
 }
