cd9919ac16d2a10f09997711619ac0f25d281031
filonich-vlad <filonich.v@mail.ru>
2019-07-19 12:02:01 +0300

    1. Конвертация листа массивов в один лист Integer[#94551]

--- chapter_003/src/test/java/ru.job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru.job4j/list/ConvertList2ArrayTest.java
@@ -2,7 +2,9 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -48,4 +50,33 @@ public class ConvertList2ArrayTest {
         };
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void when2ArraysMergedInto1First() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5};
+        List<Integer> result = list.convert(
+                new ArrayList<>() { {
+                    add(array1);
+                    add(array2);
+                } });
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when2ArraysMergedInto1Second() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[] array1 = {1, 2};
+        int[] array2 = {3, 4, 5, 6};
+        List<Integer> result = list.convert(
+                new ArrayList<>() { {
+                    add(array1);
+                    add(array2);
+                } });
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(result, is(expect));
+    }
+
 }
